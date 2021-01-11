6b89e368f3e97c08cbd99ef55a36b91278e847b7
Лев Бондаренко <lion.bond93@gmail.com>
2021-01-11 23:46:49 +0500

    add list

--- simple-objc/simple-objc/Services/NetworkService.m
+++ simple-objc/simple-objc/Services/NetworkService.m
@@ -16,15 +16,25 @@
 - (void)get:(NSString *)url {
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
         [request setHTTPMethod:@"GET"];
-//        [request setHTTPBody:body];
-//        [request setValue:[NSString stringWithFormat:@"%d", [body length]] forHTTPHeaderField:@"Content-Length"];
         [request setURL:[NSURL URLWithString:url]];
     NSError *error = nil;
         NSHTTPURLResponse *responseCode = nil;
-    NSURLSessionDataTask *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+//    NSURLSessionDataTask *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:
+      ^(NSData * _Nullable data,
+        NSURLResponse * _Nullable response,
+        NSError * _Nullable error) {
+
+//        NSError *error = nil;
+            id object = [NSJSONSerialization
+                              JSONObjectWithData:data
+                              options:0
+                              error:&error];
+          NSLog(@"Data received: %@", object[0][@"id"]);
+    }] resume];
     if([responseCode statusCode] != 200){
-            NSLog(@"Error getting %@, HTTP status code %i", url, [responseCode statusCode]);
-//            return nil;
-        }
+        
+
+    }
 }
 @end
